97e35c9dbfaad36b5f5286046884d3e67f943fe7
Nick Carneiro <nick@wizehire.com>
2018-07-17 22:05:17 -0700

    use preferred event handling style of keycodes instead of platform-dependent key values

--- anonymize.js
+++ anonymize.js
@@ -2,7 +2,7 @@ document.body.classList.add('anon-enabled')
 
 document.addEventListener('keypress', function (e) {
   console.log(e)
-  if (e.key === 'F' && e.ctrlKey || e.key === 'f' && e.altKey) {
+  if ((e.code === 'KeyF' && e.ctrlKey && e.shiftKey) || e.code === 'KeyF' && e.altKey) {
     document.body.classList.toggle('anon-enabled')
   }
 })
