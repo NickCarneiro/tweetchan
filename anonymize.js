a50ce87282a950614b3d1e0dbe1d05b869d80439
Nick Carneiro <nickc@trillworks.com>
2018-07-17 22:06:04 -0700

    remove debugging statement

--- anonymize.js
+++ anonymize.js
@@ -1,7 +1,6 @@
 document.body.classList.add('anon-enabled')
 
 document.addEventListener('keypress', function (e) {
-  console.log(e)
   if ((e.code === 'KeyF' && e.ctrlKey && e.shiftKey) || e.code === 'KeyF' && e.altKey) {
     document.body.classList.toggle('anon-enabled')
   }
