830e616aefa78018f11b73efb4b6ee0d7eb71863
Nick Carneiro <nick@wizehire.com>
2018-07-17 21:37:37 -0700

    only apply to twitter homepage, hide more elements, toggle with ctrl-A

--- anonymize.js
+++ anonymize.js
@@ -1,29 +1,8 @@
-var hashCode = function(s) {
-  var h = 0, l = s.length, i = 0;
-  if ( l > 0 )
-    while (i < l)
-      h = (h << 5) - h + s.charCodeAt(i++) | 0
-  return h
-}
-var replaceNames = function () {
-  console.log('replacing names')
-  var nameElements = document.getElementsByClassName('fullname')
-  for (var i = 0; i < nameElements.length; i++) {
-    var nameElement = nameElements[i]
-    console.log(nameElement.innerHTML)
-    var hashInteger = Math.abs(hashCode(nameElement.innerHTML))
-    var wordListSize = 2048
-    var wordIndex = hashInteger % wordListSize
-    nameElement.innerHTML = window.wordList[wordIndex]
+document.body.classList.add('anon-enabled')
+
+document.addEventListener('keypress', function (e) {
+  console.log(e)
+  if (e.key === 'a' && e.ctrlKey) {
+    document.body.classList.toggle('anon-enabled')
   }
-  var userNameElements = document.getElementsByClassName('username')
-  for (var nameElement of userNameElements) {
-    nameElement.innerHTML = ''
-  }
-}
-
-var removeChecks = function () {
-
-}
-window.onload = replaceNames
-
+})
